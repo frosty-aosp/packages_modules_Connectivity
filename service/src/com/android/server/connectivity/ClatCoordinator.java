38abe2b127d4b6e3d740079cda669f0a6572a54e
Michael Bestas <mkbestas@gmail.com>
2026-04-18 03:28:16 +0300

    ClatCoordinator: Use Cloudflare DNS instead of Google DNS
    Change-Id: I6a7544218e1be5b815ef269d7227deba9a7e9b71

--- service/src/com/android/server/connectivity/ClatCoordinator.java
+++ service/src/com/android/server/connectivity/ClatCoordinator.java
@@ -90,7 +90,7 @@ public class ClatCoordinator {
     static final String INIT_V4ADDR_STRING = "192.0.0.4";
     @VisibleForTesting
     static final int INIT_V4ADDR_PREFIX_LEN = 29;
-    private static final InetAddress GOOGLE_DNS_4 = InetAddress.parseNumericAddress("8.8.8.8");
+    private static final InetAddress GOOGLE_DNS_4 = InetAddress.parseNumericAddress("1.1.1.1");
 
     private static final int INVALID_IFINDEX = 0;
 
