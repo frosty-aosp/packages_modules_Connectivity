bfa319e4569f6e6a87389f77598ae942225efef3
SirRGB <sirrgb@proton.me>
2026-05-27 20:52:23 +0200

    Use cloudflare infra for Connectivity checks
    * ref https://github.com/amyROM/frameworks_base/commit/d1056d9448643c733150b9b981c9383d275f9062

--- tests/unit/java/com/android/server/ConnectivityServiceTest.java
+++ tests/unit/java/com/android/server/ConnectivityServiceTest.java
@@ -7753,7 +7753,7 @@ public class ConnectivityServiceTest {
     @Test
     public void testGetCaptivePortalServerUrl() throws Exception {
         String url = mCm.getCaptivePortalServerUrl();
-        assertEquals("http://connectivitycheck.gstatic.com/generate_204", url);
+        assertEquals("http://cp.cloudflare.com/generate_204", url);
     }
 
     private static class TestNetworkPinner extends NetworkPinner {
